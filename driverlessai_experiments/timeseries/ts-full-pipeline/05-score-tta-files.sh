6c28bf6dd18aa7df264087e49b5e29cf9dbdfb42
Hemen Kapadia <hemen.kapadia@gmail.com>
2019-08-05 17:29:33 -0700

    Sleep 10 seconds allowing HTTP server to start

--- driverlessai_experiments/timeseries/ts-full-pipeline/05-score-tta-files.sh
+++ driverlessai_experiments/timeseries/ts-full-pipeline/05-score-tta-files.sh
@@ -180,6 +180,7 @@ score_tta_files_using_api(){
     pushd "${scoring_data_dir}" > /dev/null &&
         source activate "${conda_env_name}" &&
         (python  "${exp_run_dir}/scoring-pipeine/http_server.py --port=9090" > /dev/null 2>&1 &) &&
+        sleep 10 &&
         python "${script_dir}/${process_script}" -n "${experiment_name}" \
                                                  -t "${script_dir}/${experiment_data_dir}/test.pickle" \
                                                  -g "${script_dir}/${experiment_data_dir}/gap.pickle" \
